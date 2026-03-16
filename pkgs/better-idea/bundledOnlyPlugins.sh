8e2978d57c07b2034b4034193acaaad8289e17cc
DavidArsene <80218600+DavidArsene@users.noreply.github.com>
2026-03-16 14:21:08 +0200

    Script works now

--- pkgs/better-idea/bundledOnlyPlugins.sh
+++ pkgs/better-idea/bundledOnlyPlugins.sh
@@ -2,26 +2,55 @@
 
 set -euo pipefail
 shopt -s failglob
-ARCHIVE="$1"
+INPUT="$1"
 
-if [[ -z "$ARCHIVE" ]]; then
-	echo "Usage: $0 </path/to/ide.tar.gz>"
+if [[ -z "$INPUT" ]]; then
+	echo "Usage: $0 </path/to/ide.tar.gz> or </path/to/ide/dir>"
 	exit 1
 fi
 
-INFO_JSON="$(tar -xOzf "$ARCHIVE" --wildcards "*-*/product-info.json")"
+if [[ -d "$INPUT" ]]; then
+	INFO_JSON="$(cat "$INPUT/product-info.json")"
+else
+	INFO_JSON="$(tar -xOzf "$INPUT" --wildcards "*-*/product-info.json")"
+fi
 getJson() { jq --raw-output "$1" <<< "$INFO_JSON"; }
 
-PLUGIN_API_BASE="https://plugins.jetbrains.com/api/search/updates/compatible?build=$(getJson .productCode)-$(getJson .buildNumber)"
+echo "All bundled plugins are:"
+declare -A "LOCATIONS=($(getJson 'reduce
+	(.layout[] | select(.kind == "plugin")) as $it
+	({};
+	. + { (
+		$it.name
+	): (
+		$it.classPath[0] | sub(
+			"plugins/(?<dir>.*)/lib/.*jar"; "\(.dir)"
+		)
+	) })
+	| to_entries
+	| map("[\(.key)]=\(.value)")
+	[]
+' | tee /dev/stderr))"
 
-echo "Checking marketplace availability of bundled plugins for $(getJson .name) $(getJson .version) ($(getJson .buildNumber))..."
+NAME="$(getJson .name)"
+VERSION="$(getJson .version)"
+PC="$(getJson .productCode)"
+BN="$(getJson .buildNumber)"
+
 echo
+echo "Checking marketplace availability of bundled plugins for $NAME $VERSION ($PC-$BN)..."
 
-# shellcheck disable=SC2016
-getJson '.bundledPlugins[]' \
-  | grep -v -e 'javaee' -e 'spring' -e 'Lombook' -e 'colorscheme' \
-  | split -l 10 --filter="sd '^(.+)$' '&pluginXmlId=\$1' | xargs -I {} -- xh -v GET '$PLUGIN_API_BASE{}'"
-#  | split -l 10 --filter="xh -v $PLUGIN_API_BASE"'$(sd "^(.+)$" "&pluginXmlId=\$1")'
+BUNDLED_PLUGINS=$(getJson '.bundledPlugins[]' | grep -v -e 'javaee' -e 'spring' -e 'Lombook' -e 'colorscheme')
 
+RESPONSE=$(xh "https://plugins.jetbrains.com/api/search/compatibleUpdates" \
+	build="$PC-$BN" \
+	pluginXMLIds:="[$(sd '^(.*)$' '"$1",' <<< "$BUNDLED_PLUGINS")0]")
+# '0' is added to eat the trailing comma
 
-#  | parallel --pipe --block 10 --line-buffer "xh -v \"$PLUGIN_API_BASE\$(sd '^(.+)$' '&pluginXmlId=\$1')\""
+echo "The following plugins were not included in the response:"
+echo
+INCLUDED="$(jq --raw-output '.[] | .pluginXmlId' <<< "$RESPONSE")"
+
+while IFS= read -r plugin; do
+	grep -q "$plugin" <<< "$INCLUDED" || echo "$plugin (plugins/${LOCATIONS["$plugin"]})"
+done <<< "$BUNDLED_PLUGINS"
